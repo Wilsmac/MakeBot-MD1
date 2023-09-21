c319dcb637ad74cbd38bf97ecc1f5c70cbee8aeb
WilSMaC <134551880+Wilsmac@users.noreply.github.com>
2023-09-21 01:05:20 -0600

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${global.packname} ⁝⁝ ✅ ${uptime} ⌛ ⁝⁝ 𓃠 ${lenguajeGB.lenguaje() == 'es' ? '#estado #menu #serbot #grupos #creadora' : '#status #menu #jadibot #groupsgb #owner'} 💻` 
+let bio = `${global.packname} ⁝⁝ 💮 ${uptime} ⌛ ⁝⁝ 🌺 ${lenguajeGB.lenguaje() == 'es' ? ' #menu #serbot #contacto #creador by wilmer' : ' #menu #jadibot #contact #owner by wilmer'} 😼❤️‍🩹` 
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
