8143e2bbda346ed8d62ce870f73a943dfa180171
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-09-24 08:47:13 -0600

    Update and rename rol-rpg_pareja_aceptar.js to fun-formarpareja.js

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -5,7 +5,7 @@ let a = ps.getRandom()
 let b
 do b = ps.getRandom()
 while (b === a)
-m.reply(`*${toM(a)}, 𝑌𝐴 𝐸𝑆 𝐻𝑂𝑅𝐴 𝐷𝐸 𝑄𝑈𝐸 𝑇𝐸 💍 𝐶𝐴𝑆𝐸𝑆 𝐶𝑂𝑁 ${toM(b)}, 𝐿𝐼𝑁𝐷𝐴 𝑃𝐴𝑅𝐸𝐽𝐴 🥰💓*
+m.reply(`*${toM(a)}, 𝑌𝐴 𝐸𝑆 𝐻𝑂𝑅𝐴 𝐷𝐸 𝑄𝑈𝐸 𝑇𝐸 💍😻 𝐶𝐴𝑆𝐸𝑆 𝐶𝑂𝑁 ${toM(b)}, 𝐿𝐼𝑁𝐷𝐴 𝑃𝐴𝑅𝐸𝐽𝐴 🥰💓*
 
 *${toM(a)}, 𝐼𝑇'𝑆 𝐴𝐵𝑂𝑈𝑇 𝑇𝐼𝑀𝑅 𝑌𝑂𝑈 💍 𝑀𝐴𝑅𝑅𝐼 ${toM(b)}, 𝐶𝑈𝑇𝐸 𝑃𝑂𝑈𝑃𝐿𝐸 😍💓*`, null, {
 mentions: [a, b]
