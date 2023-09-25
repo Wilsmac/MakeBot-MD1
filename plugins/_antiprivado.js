9b385cc1c51c03ec11cccc9b4426427194766bb3
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-09-24 20:05:27 -0600

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -3,7 +3,7 @@
 export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 if (m.isBaileys && m.fromMe) return !0
-if (m.isGroup) return !1
+if 'https://chat.whatsapp.com/' return !1
 if (!m.message) return !0 
 if (m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('deletesesion') || m.text.includes('estado') || m.text.includes('bots')) return !0
 let chat = global.db.data.chats[m.chat]
